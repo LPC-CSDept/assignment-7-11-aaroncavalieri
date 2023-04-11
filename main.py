0820bde64aca4d3fb52b226d91c299c52c00ecfb
Your Name/Applications/anaconda3/bin/python <you@example.com>
2023-04-11 16:09:22 -0700

    V01

--- main.py
+++ main.py
@@ -10,9 +10,19 @@ width = 0.2
 labels = ['Math', 'English', 'Physics', 'Computer']
 names = ['Bill', 'Mary']
 
-# ******************************
-# Make your code
-# ******************************
+fig, ax = plt.subplots()
+bar1 = ax.bar(x-width*1.5, Math, width)
+bar2 = ax.bar(x-width*0.5, English, width)
+bar3 = ax.bar(x+width*1.5, Physics, width)
+bar4 = ax.bar(x+width*0.5, Computer, width)
+
+ax.set_title("Grouped graph for scores")
+ax.bar_label(bar1, padding=3)
+ax.bar_label(bar2, padding=3)
+ax.bar_label(bar3, padding=3)
+ax.bar_label(bar4, padding=3)
+ax.legend(labels)
+ax.set_xticks(x, names)
 
 
 fig.savefig('A11.png')
